51547311e5e516ddbef6ab83762b5411dfb1e33f
Kevin Tokheim <katokh89@gmail.com>
2015-09-18 17:36:07 -0500

    add the Pizzeria.html page as well as more accurate comments in scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -9,30 +9,18 @@ function Pizza(quantity, size, topping, price){
 };
 
 //Method for returning the number of toppings
-//One topping is worth 1 dollar
 Pizza.prototype.pricePerTopping = function(){
     return this.topping * 1;
-    // return this.topping = 1;
 };
-// //Constructs the Topping object
-// function Topping(toppings){
-//     this.toppings = toppings;
-// };
-//
-// //Method for returning the price of one topping
-// Topping.prototype.pricePerTopping = function(){
-//     return this.toppings = 1;
-// };
 
-//Method for returning the price per pizza.
-//One pizza is 5 dollars
+//Method for returning the quantity of pizzas.
 Pizza.prototype.pricePerQuantity = function(){
     return this.quantity * 1;
 };
 
 //Method for returning the price per size of pizza
 //Small pizza is 5, Medium is 7.5, Large is 10
-Pizza.prototype.pricePerSize = function(){
+var pizzaSize = Pizza.prototype.pricePerSize = function(){
     if (this.size == "Small"){
         return 5;
     } else if (this.size == "Medium"){
@@ -43,8 +31,6 @@ Pizza.prototype.pricePerSize = function(){
         alert("That's a big pizza");
     }
 };
-
-
 
 //Method for returning to the total price of a pizza
 //Including quantity, size and toppings
@@ -59,9 +45,32 @@ Pizza.prototype.pizzaPrice = function(){
         alert("That's a big pizza");
     }
 
-    // this.topping = 1;
-
     this.price = (this.size + this.topping) * this.quantity;
-    console.log(this.size);
+
     return this.price;
 };
+
+
+$(document).ready(function(){
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
